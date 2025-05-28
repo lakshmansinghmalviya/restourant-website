983d43efc263da8c0dbbe205b44f4252b73d1071
Lakshman Singh <lslakshman2024@gmail.com>
2025-05-28 09:04:44 +0530

    fix the bug

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,19 +1,11 @@
-import Hero from "./components/Hero"
-import Menu from "./components/menu/Menu"
-import Testimonals from "./components/Testimonals"
-import MeetChefs from "./components/MeetChefs"
-import MeetOurTeam from "./components/MeetOurTeam"
-import Achievements from "./components/Achievements"
-import OurStory from "./components/OurStory"
 import CategoryMenu from "./components/CategoryMenu"
-import BookTable from "./components/BookTable"
+import Hero from "./components/Hero"
+import MeetChefs from "./components/MeetChefs"
+import Testimonals from "./components/Testimonals"
 
-import WeeklyDelights from './components/weekly-delights/Index'
-import BestSellers from './components/chef-best-sellers/Index'
-import PrivateEvents from './components/private-events/Index'
-import DrinkAndEnjoy from './components/DrinkAndEnjoy'
 import AllFoodNamesWithImages from './components/AllFoodNamesWithImages'
-import RestaurantHeroCarousel from './components/RestaurantHeroCarousel'
+import DrinkAndEnjoy from './components/DrinkAndEnjoy'
+import WeeklyDelights from './components/weekly-delights/Index'
 
 
 export default function Home() {
